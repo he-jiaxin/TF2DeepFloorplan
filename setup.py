cff434c04d65ba9c1786e4677611b8d44ee73200
Jiaxin He <hejiaxin.jh@gmail.com>
2024-06-13 17:21:34 +0100

    chore: update tensorflow dependency based on platform (M silicone)

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import os
-
+import sys
 from setuptools import setup
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
+
+if sys.platform == "darwin":
+    required.append("tensorflow-macos")
+    required.append("tensorflow-metal")
+else:
+    required.append("tensorflow-gpu==2.3.0")
 
 if __name__ == "__main__":
     use_scm_version = not os.environ.get("AM_I_IN_A_DOCKER_CONTAINER", False)
